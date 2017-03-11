7e9bd7c7f24c4b0542112a0c251efffd7148003e
Hayden Hughes <mrhaydenhughes@gmail.com>
2017-03-11 19:53:16 +1100

    Added setup.py and PyYacht

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PyYacht',
+      version='0.1',
+      description='Yacht race starter',
+      url='TBA',
+      author='Hayden Hughes',
+      author_email='mrhaydenhughes@gmail.com',
+      license='MIT',
+      scripts=['bin/PyStart.py'],
+      zip_safe=False)
